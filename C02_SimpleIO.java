c31991ca8c9e64356aa7d24d6695b7216016e8f1
Yi-Chang Chen <ycc.tw.email@gmail.com>
2016-10-03 23:17:08 +0800

    del failed upload

--- C02_SimpleIO.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-public class C02_SimpleIO {
-	public static void main (String[] arg) throws Exception {
-		BufferedReader keybd = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(keybd.readLine());
-	}
-}
