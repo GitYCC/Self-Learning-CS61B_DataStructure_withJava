393ebb7563c0ec695d8e74a54d9a817b1bf7c9a5
Yi-Chang Chen <ycc.tw.email@gmail.com>
2016-10-10 19:54:47 +0800

    add comment

--- CH2/C02_WWWReader.java
+++ CH2/C02_WWWReader.java
@@ -4,8 +4,11 @@ import java.io.*;
 public class C02_WWWReader {
 	public static void main (String[] arg) throws Exception{
 		URL u = new URL("https://www.google.com.tw/");
+		// InputStream Object: read raw data from some source (here is web), but don't format the data
 		InputStream ins = u.openStream();
+		// InputStreamReader Object: compose the raw data into charactors
 		InputStreamReader isr = new InputStreamReader(ins);
+		// BufferedReader Object: compoase the characters into entire lines of text
 		BufferedReader contents = new BufferedReader(isr);
 		System.out.println(contents.readLine());
 		
